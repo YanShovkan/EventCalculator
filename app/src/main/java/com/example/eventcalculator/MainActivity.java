fa87a5f124cbc576971ea4cf06af546c92722f5e
YanShovkan <Shovkanyan73@mail.ru>
2021-10-08 18:23:29 +0400

    update window PlanEvent

--- app/src/main/java/com/example/eventcalculator/MainActivity.java
+++ app/src/main/java/com/example/eventcalculator/MainActivity.java
@@ -8,13 +8,23 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonCreateEvent = (Button)findViewById(R.id.buttonCreateEvent);
+    private Button buttonPlanEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        buttonPlanEvent = (Button)findViewById(R.id.buttonPlanEvent);
+
+        buttonPlanEvent.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, Event.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
