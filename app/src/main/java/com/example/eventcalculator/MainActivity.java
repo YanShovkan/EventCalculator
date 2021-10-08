d68c7943f4c07c39de12f53744f7f70f7379e9d4
YanShovkan <Shovkanyan73@mail.ru>
2021-10-08 17:08:07 +0400

    activity

--- app/src/main/java/com/example/eventcalculator/MainActivity.java
+++ app/src/main/java/com/example/eventcalculator/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.eventcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonCreateEvent = (Button)findViewById(R.id.buttonCreateEvent);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
