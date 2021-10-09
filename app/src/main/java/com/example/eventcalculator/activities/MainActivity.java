9d75fda29a277c95f1a25bad12a71c4d974fc8a0
pSavin1337 <p.savin73@gmail.com>
2021-10-10 00:11:10 +0400

    add logic

--- app/src/main/java/com/example/eventcalculator/activities/MainActivity.java
+++ app/src/main/java/com/example/eventcalculator/activities/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.eventcalculator.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.example.eventcalculator.R;
+import com.example.eventcalculator.database.DatabaseHelper;
 import com.example.eventcalculator.database.Storages.ProductStorage;
 import com.example.eventcalculator.eventBusinessLogic.businessLogic.ProductLogic;
 import com.example.eventcalculator.eventBusinessLogic.models.ProductModel;
@@ -17,11 +21,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonPlanEvent;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    SimpleCursorAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
 
         ProductStorage productStorage = new ProductStorage(this);
         ProductLogic productLogic = new ProductLogic(productStorage);
@@ -50,4 +60,28 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // открываем подключение
+        db = databaseHelper.getReadableDatabase();
+
+        //получаем данные из бд в виде курсора
+        userCursor =  db.rawQuery("select * from "+ DatabaseHelper.TABLE, null);
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[] {DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        // Закрываем подключение и курсор
+        db.close();
+        userCursor.close();
+    }
+
 }
