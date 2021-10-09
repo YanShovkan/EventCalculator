255930a1d18f3a6dc433b816bf9bd072912de841
YanShovkan <Shovkanyan73@mail.ru>
2021-10-09 17:04:15 +0400

    table

--- app/src/main/java/com/example/eventcalculator/activities/Products.java
+++ app/src/main/java/com/example/eventcalculator/activities/Products.java
@@ -2,29 +2,77 @@ package com.example.eventcalculator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.eventcalculator.R;
 import com.example.eventcalculator.helperClasses.ViewTables;
 
 public class Products extends AppCompatActivity {
-    ViewTables<Product> table = new ViewTables<Product>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
 
-        table.fillTable(new String[] {"name", "surname"},new Product[]{ new Product("yan", "shovkan"), new Product("Vald", "Reznikov")} , this);
     }
-}
-class Product{
 
-    String name;
-    String surname;
+    void fillTable(String[] titles, Product[] products) {
+        TableLayout tableLayoutProducts = findViewById(R.id.tableLayoutProducts);
 
-    Product(String name, String surname){
-        this.name = name;
-        this.surname = surname;
+        TableRow tableRowTitles = new TableRow(this);
+
+        for (String title : titles) {
+            TextView textView = new TextView(this);
+
+            textView.setText(title);
+            textView.setTextColor(Color.WHITE);
+            textView.setGravity(Gravity.CENTER);
+            textView.setWidth(250);
+            tableRowTitles.addView(textView);
+        }
+
+        tableRowTitles.setBackgroundColor(Color.parseColor("#FF6200EE"));
+        tableLayoutProducts.addView(tableRowTitles);
+
+        for (Product product : products) {
+            TableRow tableRow = new TableRow(this);
+
+            TextView textViewName = new TextView(this);
+            textViewName.setText(Product.name.toString());
+            textViewName.setTextColor(Color.WHITE);
+            textViewName.setGravity(Gravity.CENTER);
+
+            TextView textViewCost = new TextView(this);
+            textViewCost.setText(Product.cost.toString());
+            textViewCost.setTextColor(Color.WHITE);
+            textViewCost.setGravity(Gravity.CENTER);
+
+            TextView textViewCount = new TextView(this);
+            textViewCount.setText(Product.count.toString());
+            textViewCount.setTextColor(Color.WHITE);
+            textViewCount.setGravity(Gravity.CENTER);
+
+            tableRow.addView(textViewName);
+            tableRow.addView(textViewCost);
+            tableRow.addView(textViewCount);
+
+            tableRow.setBackgroundColor(Color.parseColor("#FF6200EE"));
+
+
+            tableRow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            tableLayoutProducts.addView(tableRow);
+        }
     }
 }
