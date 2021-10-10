edc1fe73aa47065bc60064c7dc1dd15f2892251a
Vladis-01 <vladis.a.-01@mail.ru>
2021-10-10 13:51:13 +0400

    add handout

--- app/src/main/java/com/example/eventcalculator/activities/Handouts.java
+++ app/src/main/java/com/example/eventcalculator/activities/Handouts.java
@@ -2,15 +2,119 @@ package com.example.eventcalculator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.eventcalculator.R;
+import com.example.eventcalculator.eventBusinessLogic.models.HandoutModel;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Handouts extends AppCompatActivity {
+
+    TableRow selectedRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handouts);
+        selectedRow = new TableRow(this);
+
+        fillTable(Arrays.asList("Название", "Стоимость за штуку", "Количество на человека"), getHandouts());
+
     }
+
+    void fillTable(List<String> titles, List<HandoutModel> handouts) {
+
+        TableLayout tableLayoutHandouts = findViewById(R.id.tableLayoutHandouts);
+
+        TableRow tableRowTitles = new TableRow(this);
+
+        for (String title : titles) {
+            TextView textView = new TextView(this);
+
+            textView.setTextSize(16);
+            textView.setText(title);
+            textView.setTextColor(Color.WHITE);
+            textView.setGravity(Gravity.CENTER);
+            textView.setWidth( getWindowManager().getDefaultDisplay().getWidth() / 3);
+            tableRowTitles.addView(textView);
+        }
+
+        tableRowTitles.setBackgroundColor(Color.parseColor("#FF6200EE"));
+        tableLayoutHandouts.addView(tableRowTitles);
+
+
+        for (HandoutModel premise : handouts) {
+            TableRow tableRow = new TableRow(this);
+
+            TextView textViewName = new TextView(this);
+            textViewName.setText(premise.name);
+            textViewName.setHeight(170);
+            textViewName.setTextSize(16);
+            textViewName.setTextColor(Color.WHITE);
+            textViewName.setGravity(Gravity.CENTER);
+
+            TextView textViewCost = new TextView(this);
+            textViewName.setHeight(170);
+            textViewCost.setTextSize(16);
+            textViewCost.setText(String.valueOf(premise.price));
+            textViewCost.setTextColor(Color.WHITE);
+            textViewCost.setGravity(Gravity.CENTER);
+
+            TextView textViewCount = new TextView(this);
+            textViewName.setHeight(170);
+            textViewCount.setTextSize(16);
+            textViewCount.setText(String.valueOf(premise.countPerPeople));
+            textViewCount.setTextColor(Color.WHITE);
+            textViewCount.setGravity(Gravity.CENTER);
+
+            TextView textViewId = new TextView(this);
+            textViewId.setVisibility(View.INVISIBLE);
+            textViewId.setText(String.valueOf(premise.id));
+
+            tableRow.addView(textViewName);
+            tableRow.addView(textViewCost);
+            tableRow.addView(textViewCount);
+
+            tableRow.setBackgroundColor(Color.parseColor("#FF6200EE"));
+
+            tableRow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    selectedRow = tableRow;
+
+                    for(int i = 0; i < tableLayoutHandouts.getChildCount(); i++){
+                        View view = tableLayoutHandouts.getChildAt(i);
+                        if (view instanceof TableRow ){
+                            view.setBackgroundColor(Color.parseColor("#FF6200EE"));
+                        }
+                    }
+
+                    tableRow.setBackgroundColor(Color.parseColor("#FF6200FF"));
+                }
+            });
+
+            tableLayoutHandouts.addView(tableRow);
+        }
+    }
+
+    List<HandoutModel> getHandouts(){
+        List<HandoutModel> handouts = new LinkedList<HandoutModel>();
+
+        for(int i = 0; i < 10; i++){
+            handouts.add(new HandoutModel("name" + i,i*10,i*2,1));
+        }
+
+        return  handouts;
+    }
+
 }
---
+++ app/src/main/java/com/example/eventcalculator/database/Storages/HandoutStorage.java
@@ -0,0 +1,119 @@
+package com.example.eventcalculator.database.Storages;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.eventcalculator.database.DatabaseHelper;
+import com.example.eventcalculator.eventBusinessLogic.models.HandoutModel;
+import com.example.eventcalculator.eventBusinessLogic.models.ProductModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandoutStorage {
+    DatabaseHelper sqlHelper;
+    SQLiteDatabase db;
+    final String TABLE = "handout";
+    final String COLUMN_ID = "handoutid";
+    final String COLUMN_NAME = "handout_name";
+    final String COLUMN_PRICE = "handout_cost";
+    final String COLUMN_COUNTPERPEOPLE = "count_per_person";
+    final String COLUMN_EVENTID = "eventid";
+
+    public HandoutStorage(Context context) {
+        sqlHelper = new DatabaseHelper(context.getApplicationContext());
+        db = sqlHelper.getWritableDatabase();
+    }
+
+    public HandoutStorage open() {
+        db = sqlHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public List<HandoutModel> getFullList() {
+        Cursor cursor = db.rawQuery("select * from " + TABLE, null);
+        List<HandoutModel> list = new ArrayList<>();
+        if (!cursor.moveToFirst()) {
+            return list;
+        }
+
+        do {
+            HandoutModel obj = new HandoutModel();
+            obj.id = cursor.getInt((int) cursor.getColumnIndex(COLUMN_ID));
+            obj.price = cursor.getInt((int) cursor.getColumnIndex(COLUMN_PRICE));
+            obj.countPerPeople = cursor.getInt((int) cursor.getColumnIndex(COLUMN_COUNTPERPEOPLE));
+            obj.eventId = cursor.getInt((int) cursor.getColumnIndex(COLUMN_EVENTID));
+            obj.name = cursor.getString((int) cursor.getColumnIndex(COLUMN_NAME));
+
+            list.add(obj);
+            cursor.moveToNext();
+        } while (!cursor.isAfterLast());
+        return list;
+    }
+
+    public List<HandoutModel> getFilteredList(HandoutModel model) {
+        Cursor cursor = db.rawQuery("select * from " + TABLE + " where "
+                + COLUMN_EVENTID + " = " + model.eventId, null);
+        List<HandoutModel> list = new ArrayList<HandoutModel>();
+        if (!cursor.moveToFirst()) {
+            return list;
+        }
+        do {
+            HandoutModel obj = new HandoutModel();
+            obj.id = cursor.getInt((int) cursor.getColumnIndex(COLUMN_ID));
+            obj.price = cursor.getInt((int) cursor.getColumnIndex(COLUMN_PRICE));
+            obj.countPerPeople = cursor.getInt((int) cursor.getColumnIndex(COLUMN_COUNTPERPEOPLE));
+            obj.eventId = cursor.getInt((int) cursor.getColumnIndex(COLUMN_EVENTID));
+            obj.name = cursor.getString((int) cursor.getColumnIndex(COLUMN_NAME));
+
+            list.add(obj);
+            cursor.moveToNext();
+        } while (!cursor.isAfterLast());
+        return list;
+    }
+
+    public HandoutModel getElement(HandoutModel model) {
+        Cursor cursor = db.rawQuery("select * from " + TABLE + " where "
+                + COLUMN_ID + " = " + model.id, null);
+        HandoutModel obj = new HandoutModel();
+        if (!cursor.moveToFirst()) {
+            return null;
+        }
+        obj.id = cursor.getInt((int) cursor.getColumnIndex(COLUMN_ID));
+        obj.price = cursor.getInt((int) cursor.getColumnIndex(COLUMN_PRICE));
+        obj.countPerPeople = cursor.getInt((int) cursor.getColumnIndex(COLUMN_COUNTPERPEOPLE));
+        obj.eventId = cursor.getInt((int) cursor.getColumnIndex(COLUMN_EVENTID));
+        obj.name = cursor.getString((int) cursor.getColumnIndex(COLUMN_NAME));
+        return obj;
+    }
+
+    public void insert(HandoutModel model) {
+        ContentValues content = new ContentValues();
+        content.put(COLUMN_PRICE,model.price);
+        content.put(COLUMN_COUNTPERPEOPLE,model.countPerPeople);
+        content.put(COLUMN_EVENTID,model.eventId);
+        content.put(COLUMN_NAME,model.name);
+        db.insert(TABLE,null,content);
+    }
+
+    public void update(HandoutModel model) {
+        ContentValues content=new ContentValues();
+        content.put(COLUMN_PRICE,model.price);
+        content.put(COLUMN_COUNTPERPEOPLE,model.countPerPeople);
+        content.put(COLUMN_EVENTID,model.eventId);
+        content.put(COLUMN_NAME,model.name);
+        String where = COLUMN_ID+" = "+model.id;
+        db.update(TABLE,content,where,null);
+    }
+
+    public void delete(HandoutModel model) {
+        String where = COLUMN_ID+" = "+model.id;
+        db.delete(TABLE,where,null);
+    }
+}
